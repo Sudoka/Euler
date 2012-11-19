c3d876ff82c39c523f8effda6f781196e2f9f864
Amrit Kallu <amrit.kallu@gmail.com>
2012-11-18 23:36:23 -0800

    changed euler.py from hosting an euler object to just being a file with methods

--- euler.py
+++ euler.py
@@ -3,28 +3,23 @@ import re
 import sys
 import time
 
-class euler:
+#takes parameter n, and finds primes up to and including n
+def primes(n):
+  mults = set()
+  primes = []
+  for i in xrange(2, n+1):
+    if i not in mults:
+      primes.append(i)
+      mults.update(xrange(i*i, n+1, i))
+  return primes
+#returns the nth fibonacci number
+def nth_fib(self, n):
+  return int(round((1.6180339**n - (-0.6180339)**n)/2.236067))
 
-  def add(self, x, y):
-    return x+y
-
-  #takes parameter n, and finds primes up to and including n
-  def primes(self, n):
-    mults = set()
-    primes = []
-
-    for i in xrange(2, n+1):
-        if i not in mults:
-            primes.append(i)
-            mults.update(xrange(i*i, n+1, i))
-    return primes
-
-  #returns the nth fibonacci number
-  def nth_fib(self, n):
-    return int(round((1.6180339**n - (-0.6180339)**n)/2.236067))
-  
-
-
-# Standard boilerplate to call the main() function.
-if __name__ == '__main__':
-  main()
+#returns the greatest common denominator (euclid's algorithm)
+def gcd(a,b):
+  if b > a:
+    (a, b) = (b, a)
+  while b != 0:
+    (a, b) = (b, a%b)
+  return a
