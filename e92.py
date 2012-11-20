b637b6cab0cb23766488627be3c5af60dfd814dc
Amrit Kallu <amrit.kallu@gmail.com>
2012-11-19 23:39:59 -0800

    added e92.py

---
+++ e92.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python -tt
+import re
+import sys
+from euler import *
+from time import time
+
+def main():
+  ones = set()
+  ones.add(1)
+  ens = set()
+  ens.add(89)
+  for i in xrange(1, 10000000):
+    nums = []
+    n = i
+    nums.append(n)
+    while not n in ens and not n in ones:
+      n = next_num(n)
+      nums.append(n)
+    if n in ens:
+      for num in nums:
+        ens.add(num)
+    elif n in ones:
+      for num in nums:
+        ones.add(num)
+
+
+  print len(ens)
+    
+    
+
+# given a number n, sum up and square each of the digits
+def next_num(n):
+  s = str(n)
+  nextnum = 0
+  for digit in s: nextnum += int(digit)**2
+  return nextnum
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+  main()
